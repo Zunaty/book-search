23e89525cb5a5cab3040560a84729b3a847cb697
Victor Perez <zunaty@gmail.com>
2021-11-07 11:51:52 -0700

    server.js update, typeDefs, and resolvers, started

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+
+const resolvers = {
+    Query: {
+
+    },
+
+    Mutation: {
+
+    }
+};
+
+module.exports = resolvers;
